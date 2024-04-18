2a17015c40762ea36941ab943c9205ca669a491a
ClaBoris <claudia.borsani14@gmail.com>
2024-04-18 12:53:17 +0200

    Ho cercato di implementare il fingerprinting ma non mi funziona correttamente su Mockoon

--- product.js
+++ product.js
@@ -10,3 +10,41 @@ fetch('http://localhost:5500/PagamentoOnline/product-to-buy')
     // Gestisci gli errori di connessione
     console.error('Errore durante il recupero dei dati:', error);
   });
+
+
+  /*FINGERPRINTING*/
+  // fingerprint.js
+
+function generateFingerprint() {
+  var fingerprint = "";
+
+  // Aggiungi informazioni uniche sul dispositivo o sul browser
+  fingerprint += "Browser: " + navigator.userAgent + "\n";
+  fingerprint += "Screen size: " + screen.width + "x" + screen.height + "\n";
+
+   // Invia i dati al server
+   fetch('http://localhost:5500/PagamentoOnline/product-to-buy', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ fingerprint: fingerprint })
+  })
+  .then(response => {
+      if (!response.ok) {
+          throw new Error('Network response was not ok');
+      }
+     return response.json();
+  })
+  .then(data => {
+      console.log('Fingerprinting data sent successfully:', data);
+  })
+  .catch(error => {
+      console.error('There was a problem sending the fingerprinting data:', error);
+  });
+
+    return fingerprint;
+}
+
+/*var fingerprint = generateFingerprint();
+console.log(fingerprint);*/
