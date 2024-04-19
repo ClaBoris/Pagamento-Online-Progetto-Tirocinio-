810fde8a439850edcebd0aec2c2dbc528d0a6e38
ClaBoris <claudia.borsani14@gmail.com>
2024-04-19 12:22:46 +0200

    Ho aggiunto gli eventi per le coordinate x,y del mouse sulla pagina product.html. Ho reinserito la GET iniziale che però attualmente non sta funzionando, mentre la POST invia correttamente i dati nel body sia del fingerprint sia degli eventi e mi scarica il file .json applicato con tutti gli eventi al suo interno.

--- product.js
+++ product.js
@@ -1,6 +1,6 @@
  /*RICHIESTA HTTP A MOCKOON*/
 
-/*fetch('http://localhost:5500/PagamentoOnline/product-to-buy')
+fetch('http://localhost:5500/PagamentoOnline/product-to-buy')
   .then(response => response.json())
   .then(data => {
     // Gestisci la risposta dalla tua API Mockoon
@@ -9,48 +9,49 @@
   .catch(error => {
     // Gestisci gli errori di connessione
     console.error('Errore durante il recupero dei dati:', error);
-  });*/
-
-
-  /*FINGERPRINTING*/
-  // ho cercato di implementare il fingerpriting. Ho creato nella richiesta get su mockoon una nuova risposta che pottesse
-  //rispondere al fingerprinting però essendo una get e questa una post, la risposta arriva ma sotto forma di get. Allora
-  //ho pensato di mette al post di get "All request" ma comunque sempre sotto forma di get torna. Vorrei metterla sotto forma 
-  //di POST. Vedere come farlo!!!
-
-/*function generateFingerprint() {
-  var fingerprint = "";
-
-  // Aggiungi informazioni uniche sul dispositivo o sul browser
-  fingerprint += "Browser: " + navigator.userAgent + "\n";
-  fingerprint += "Screen size: " + screen.width + "x" + screen.height + "\n";
-
-   // Invia i dati al server
-   fetch('http://localhost:5500/PagamentoOnline/product-to-buy', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ fingerprint: fingerprint })
-  })
-  .then(response => {
-      if (!response.ok) {
-          throw new Error('Network response was not ok');
-      }
-     return response.json();
-  })
-  .then(data => {
-      console.log('Fingerprinting data sent successfully:', data);
-  })
-  .catch(error => {
-      console.error('There was a problem sending the fingerprinting data:', error);
   });
 
-    return fingerprint;
+/***************************************GESTIONE EVENTI***************************************/
+
+//Creo un array per contenere gli eventi
+var array = [];
+
+//creo un funzione per stampare gli eventi 
+
+function print_event(event){
+   const eventDiv = document.getElementById("events");
+   eventDiv.innerHTML += "<p>" + event + "</p>";
+   array.push(event);
 }
 
-var fingerprint = generateFingerprint();
-console.log(fingerprint);*/
+
+document.getElementById("body").addEventListener('mousemove' , function(event){
+
+  var mouseX = event.clientX;
+
+  var mouseY = event.clientY; 
+
+  print_event("Coordinata X: " + mouseX +" e "+"Coordinata y: " + mouseY);
+});
+
+function saveEvents(){
+
+  var eventsJSON = JSON.stringify(array);
+
+  var blob = new Blob([eventsJSON], {type: "application/json"});
+
+  saveAs(blob, "product_events.json");
+}
+
+var download = document.getElementById("buyButton");
+
+download.addEventListener('click', function(){
+
+    saveEvents();
+});
+
+/***************************************FINGERPRINTING***************************************/
+
 
 function generateFingerprint() {
   var fingerprint = "";
@@ -59,26 +60,45 @@ function generateFingerprint() {
   fingerprint += "Browser: " + encodeURIComponent(navigator.userAgent) + "&";
   fingerprint += "Screen size: " + screen.width + "x" + screen.height;
 
-  // Costruisci l'URL con i parametri
-  var url = 'http://localhost:5500/PagamentoOnline/product-to-buy?' + fingerprint;
-
-  // Invia la richiesta GET
-  fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log('Fingerprinting data sent successfully:', data);
-    })
-    .catch(error => {
-      console.error('There was a problem sending the fingerprinting data:', error);
-    });
+  // Aggiungi informazioni sugli eventi
+  fingerprint += "Events: " + array.join(", ");
 
   return fingerprint;
 }
+ function sendFingerprintAndEvents(fingerprint){
 
-var fingerprint = generateFingerprint();
-console.log(fingerprint);
+  var dataToSend = {
+    fingerprint : fingerprint,
+    events: array
+  };
+
+  //richiesta POST
+
+  fetch('http://localhost:5500/PagamentoOnline/product-to-buy?', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(dataToSend)
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log('Data sent successfully:', data);
+    // Eventuali azioni aggiuntive dopo l'invio dei dati al server
+  })
+  .catch(error => {
+    console.error('There was a problem sending the data:', error);
+  });
+ }
+
+ // Aggiungi un event listener al pulsante per procedere con il pagamento
+var buyButton = document.getElementById("buyButton");
+buyButton.addEventListener('click', function() {
+  var fingerprint = generateFingerprint(); // Genera il fingerprinting
+  sendFingerprintAndEvents(fingerprint); // Invia il fingerprinting e gli eventi al server
+});
