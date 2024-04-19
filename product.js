d33b5024e060c7f01c0de3b720c0c5d1a33b093a
ClaBoris <claudia.borsani14@gmail.com>
2024-04-19 09:35:37 +0200

    Aggiunta della richiesta get della prima pagina (product.html) con fingerprinting a mockoon. Ho commentato la vecchia richiesta get per evitare di fare due richieste get differenti a mockoon ogni volta.

--- product.js
+++ product.js
@@ -1,6 +1,6 @@
  /*RICHIESTA HTTP A MOCKOON*/
 
-fetch('http://localhost:5500/PagamentoOnline/product-to-buy')
+/*fetch('http://localhost:5500/PagamentoOnline/product-to-buy')
   .then(response => response.json())
   .then(data => {
     // Gestisci la risposta dalla tua API Mockoon
@@ -9,13 +9,16 @@ fetch('http://localhost:5500/PagamentoOnline/product-to-buy')
   .catch(error => {
     // Gestisci gli errori di connessione
     console.error('Errore durante il recupero dei dati:', error);
-  });
+  });*/
 
 
   /*FINGERPRINTING*/
-  // fingerprint.js
+  // ho cercato di implementare il fingerpriting. Ho creato nella richiesta get su mockoon una nuova risposta che pottesse
+  //rispondere al fingerprinting però essendo una get e questa una post, la risposta arriva ma sotto forma di get. Allora
+  //ho pensato di mette al post di get "All request" ma comunque sempre sotto forma di get torna. Vorrei metterla sotto forma 
+  //di POST. Vedere come farlo!!!
 
-function generateFingerprint() {
+/*function generateFingerprint() {
   var fingerprint = "";
 
   // Aggiungi informazioni uniche sul dispositivo o sul browser
@@ -46,5 +49,36 @@ function generateFingerprint() {
     return fingerprint;
 }
 
-/*var fingerprint = generateFingerprint();
+var fingerprint = generateFingerprint();
 console.log(fingerprint);*/
+
+function generateFingerprint() {
+  var fingerprint = "";
+
+  // Aggiungi informazioni uniche sul dispositivo o sul browser
+  fingerprint += "Browser: " + encodeURIComponent(navigator.userAgent) + "&";
+  fingerprint += "Screen size: " + screen.width + "x" + screen.height;
+
+  // Costruisci l'URL con i parametri
+  var url = 'http://localhost:5500/PagamentoOnline/product-to-buy?' + fingerprint;
+
+  // Invia la richiesta GET
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('Fingerprinting data sent successfully:', data);
+    })
+    .catch(error => {
+      console.error('There was a problem sending the fingerprinting data:', error);
+    });
+
+  return fingerprint;
+}
+
+var fingerprint = generateFingerprint();
+console.log(fingerprint);
